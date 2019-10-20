875743017b7de06484969aa6b88e7b910ca07950
kobalt <>
2019-10-20 11:16:09 +0900

    first commit

--- procedure.py
+++ procedure.py
@@ -5,9 +5,9 @@ def handle_string(string):
     slist = string.split('\n')
     cmd = slist[0]
     vals = slist[1:]
-
+    
     chousei = Chlib.Chouseisan()
-
+    
     if 'create' == cmd:
         name = vals[0]
         comment = vals[1]
---
+++ test.py
@@ -0,0 +1,11 @@
+import procedure
+
+string = '''create
+test_title
+test_comment
+11
+22
+33
+44
+'''
+print(procedure.handle_string(string))
